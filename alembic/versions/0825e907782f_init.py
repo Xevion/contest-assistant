0e7bd35333fd90d1c81a44a8b17e4cb6bbc7272b
Xevion <xevioni@yandex.com>
2021-02-14 01:20:22 -0600

    alembic migrations, init & Submissions.vote column

---
+++ alembic/versions/0825e907782f_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 0825e907782f
+Revises: 
+Create Date: 2021-02-13 23:03:36.530511-06:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0825e907782f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
